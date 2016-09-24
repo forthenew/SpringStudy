5c3589f640385d19e88254ce4da683693d7ab587
forthenew <forthenew@gmail.com>
2016-09-24 16:13:46 +0900

    @SessionAttributes 테스트.

---
+++ src/main/java/com/forthenew/springstudy/web/atmvc/SessionAttributesTest.java
@@ -0,0 +1,88 @@
+package com.forthenew.springstudy.web.atmvc;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.junit.Assert.assertThat;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpSession;
+
+import org.junit.Test;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+
+import com.forthenew.springstudy.web.AbstractDispatcherServletTest;
+
+public class SessionAttributesTest extends AbstractDispatcherServletTest {
+
+	@Test
+	public void sessionAttr() throws ServletException, IOException{
+		setClasses(UserController.class);
+		//GET 요청 - form()
+		initRequest("/user/edit").addParameter("id",  "1");
+		runService();
+		
+		HttpSession session = request.getSession();
+		assertThat(session.getAttribute("user"), is(getModelAndView().getModel().get("user")));
+		
+		//POST 요청 - submit()
+		initRequest("/user/edit", "POST").addParameter("id",  "1").addParameter("name",  "Spring2");
+		request.setSession(session);
+		runService();
+		assertThat(((User)getModelAndView().getModel().get("user")).getEmail(),is("mail@spring.com"));
+		assertThat(session.getAttribute("user"), is(nullValue()));
+	}
+	
+	@Controller
+	@SessionAttributes("user")
+	static class UserController{
+		@RequestMapping(value="/user/edit", method=RequestMethod.GET)
+		public User form(@RequestParam int id){
+			return new User(1, "Spring", "mail@spring.com");
+		}
+		@RequestMapping(value="/user/edit", method=RequestMethod.POST)
+		public void submit(@ModelAttribute User user, SessionStatus sessionStatus){
+			sessionStatus.setComplete();
+		}
+	}
+	
+	static class User{
+		int id; String name; String email;
+
+		public User(){};
+		public User(int id, String name, String email){
+			this.id = id; this.name = name; this.email = email;
+		}
+		public int getId() {
+			return id;
+		}
+
+		public void setId(int id) {
+			this.id = id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getEmail() {
+			return email;
+		}
+
+		public void setEmail(String email) {
+			this.email = email;
+		}
+		
+	}
+}
